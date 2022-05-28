7a98a7872be9a936720a856418096f91eae86830
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-05-28 10:34:31 +0000

    Added states and classes

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,16 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //create your first component
 const TrafficLight = () => {
+	const [redLight, setRedLight] = useState(false);
+	const [yellowLight, setYellowLight] = useState(false);
+	const [greenLight, setGreenLight] = useState(false);
 
-	
+	const toggleRed = () => {
+		setRedLight(true);
+		setYellowLight(false);
+		setGreenLight(false);
+	};
+	const toggleYellow = () => {
+		setYellowLight(true);
+		setRedLight(false);
+		setGreenLight(false);
+	};
+	const toggleGreen = () => {
+		setGreenLight(true);
+		setRedLight(false);
+		setYellowLight(false);
+	};
+
 	return (
 		<div className="traffic-light">
-			<button className="red"></button>
+			<button
+				id="red"
+				className={redLight ? "glow" : null}
+				onClick={toggleRed}></button>
 			<br />
-			<button className="yellow"></button>
+			<button
+				id="yellow"
+				className={yellowLight ? "glow" : null}
+				onClick={toggleYellow}></button>
 			<br />
-			<button className="green"></button>
+			<button
+				id="green"
+				className={greenLight ? "glow" : null}
+				onClick={toggleGreen}></button>
 		</div>
 	);
 };
